64016c400e51ed1e5085b454e82ab7df8a9ed6b3
ThurberDaniel <phxburg@gmail.com>
2021-06-10 17:26:38 -0600

    adding months

--- Challenges/views.py
+++ Challenges/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+
+monthDictionary = {
+    "January": "Shovel Snow New Year",
+    "Febuary": " Valentines Day",
+    "March": "St Partricks Day",
+    "April": "Rainy",
+    "May": "Memorial Day",
+    "June": "Summer Time",
+    "July": "Fourth of July",
+    "August": "Slowly Fall",
+    "September": "Fall Time",
+    "October": "Halloween",
+    "November": "Thanksgiving",
+    "December": "Christmas",
+}
 
 
 def index(request):
     return HttpResponse("hello, Im working")
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthDictionary.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    forward_month = months[month-1]
+    return HttpResponseRedirect("/challenges/" + forward_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthDictionary[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This Month is not supported")
